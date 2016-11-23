beaba8de1ef11dd4f619c6d69a417741f14002d3
shaojunhui <shaojunhui@spriteapp.com>
2016-11-23 13:36:19 +0800

    review

--- static/index_main.js
+++ static/index_main.js
@@ -1,3 +1,137 @@
+var Cell = {
+    createNew: function(x, y, mine, count) {
+        var c = {
+            x: x,
+            y: y,
+            flag: false,     // 用户标记
+            count: count,    // 周围雷数
+            mine: mine,      // 自己是否是雷
+            opened: false,
+            toClassName: function() {
+                if (this.mine)
+                    return "mine";
+            }
+        }
+        return c;
+    }
+};
+
+function shuffle(a) {
+    // var myArray = [1,2,3,4,5,6,7,8,9];
+    // shuffle(myArray);
+
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+var Game = {
+    table: null,
+    cells: null,
+    mine_count: 9,
+    w: 9,
+    h: 9,
+    init: function (e) {
+        this.table = e;
+        var self = this;
+        this.table.find("td").each(function (index, e){
+            $(this).bind('click', self.cellClick);
+            $(this).bind('contextmenu', self.cellRightClick);
+            $(this).attr('id', index);
+        });
+
+        // TODO: call in "start game" button click
+        this.start();
+    },
+    die: function(){
+        for(var i=0; i<this.cells.length; i++) {
+            if (this.cells[i].mine) {
+                $('td#' + i).addClass('mine');
+            } else {
+                if(this.cells[i].count)
+                    $('td#' + i).text(this.cells[i].count);
+                $('td#' + i).addClass('count');
+            }
+        }
+    },
+    open: function(i){
+        console.log('open', i, this.cells[i]);
+        if (this.cells[i].mine || this.cells[i].opened)
+            return;
+        else if (!this.cells[i].flag) {
+            $('td#' + i).text(this.cells[i].count);
+            $('td#' + i).addClass('count');
+            this.cells[i].opened = true;
+        }
+        var around = this.around(i);
+        console.log('around', around);
+        // return;
+        var len = around.length;
+        for (var j= 0; j< len; j++) {
+            this.open(around[j]);
+        }
+    },
+    around: function(i) {
+        var self = this;
+        function incell(i) {
+            return i>=0 && i<self.w * self.h;
+        }
+        return [i-self.w, i-1, i+1, i+self.w].filter(incell);
+    },
+    cellClick: function(e) {
+        var self = Game;
+        var id = $(this).attr('id');
+        console.log($(this).attr('id'));
+        if(self.cells[id].mine) {
+            $(this).addClass('mine');
+            self.die();
+        }else {
+            $(this).addClass('count');
+            self.open(id);
+        }
+    },
+    cellRightClick: function(e) {
+        var self = Game;
+        var id = $(this).attr('id');
+        self.cells[id].flag = true;
+        $(this).addClass('flag');
+        return false;
+    },
+    start: function () {
+        this.cells = new Array();
+        var w = this.w, h = this.h;
+
+        var bombs = new Array(w * h);
+
+        for (var x=0; x<w; x++) {
+            for (var y=0; y<h; y++) {
+                var i = x+y*h;
+                bombs[i] = i;
+                this.cells.push(Cell.createNew(x, y, false, 0))
+            }
+        }
+        shuffle(bombs);
+        for (var i=0; i<this.mine_count; i++) {
+            console.log('mine', bombs[i]);
+            this.cells[bombs[i]].mine = true;
+        }
+
+        // TODO: 计算 count, flag
+
+        // TODO: interval 倒数
+    },
+};
+
+$(document).ready(function() {
+    Game.init($('#mines'));
+});
+
+
+
 var rows = 9;//9行
 var cols = 9;//9列
 var bombNumber = 10;//雷数量
@@ -9,6 +143,7 @@ arrtableId = new Array();
 
 
 $(document).ready(function() {
+    return;
 creatId();
 creatBomb();
 getArroundId(); 
